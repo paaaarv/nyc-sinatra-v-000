78845901c31b0ccf43eb9e92fc42f87dde797af9
Parv Kumar <parvathikumar92@gmail.com>
2017-09-25 22:25:09 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -18,7 +18,7 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/show'
   end
 
-  get "/landmarks/:id/edit" do 
+  get "/landmarks/:id/edit" do
     @landmark = Landmark.find(params[:id])
 
     erb :'/landmarks/edit'
