ab5e89623decb726fb992c12d7568be2d1a941a7
Parv Kumar <parvathikumar92@gmail.com>
2017-09-25 22:09:18 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -18,9 +18,11 @@ class LandmarksController < ApplicationController
     erb :'/landmarks/show'
   end
 
-  post "/landmarks" do 
+  post "/landmarks" do
     @landmark = Landmark.create(params["landmark"])
 
     redirect("/landmarks/#{@landmark.id}")
+  end 
+  
 
 end
